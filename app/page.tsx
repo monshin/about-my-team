43d5bc178dd8dcc6aef84b2b99d2bbefff714e8f
Monshin <tuc930908@gmail.com>
2025-07-02 12:15:37 +0800

    - 改為 純HTML

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-screen md:h-716">
-      <div className="page-container">
-        <h1>哈囉！我們是 MMP 團隊</h1>
-      </div>
-    </div>
-  );
-}
--- constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const IS_PROD = String(process.env.IS_PROD || '').toLowerCase() === 'true';
